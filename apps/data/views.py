8955320a5d08ee4fcd350e324320f7818cfe382e
Rob <>
2022-06-26 12:19:17 -0400

    make domain url an env var

--- apps/data/views.py
+++ apps/data/views.py
@@ -7,11 +7,12 @@ from .functions import amz_refresh_token
 import os
 
 LWA_CLIENT_ID = os.environ.get('LWA_CLIENT_ID')
+DOMAIN_URL = os.environ.get('DOMAIN_URL')
 
 
 def index(request):
     if request.user.is_authenticated:
-        return render(request, 'data/my_ads_accounts.html', context = {'LWA_CLIENT_ID': LWA_CLIENT_ID})
+        return render(request, 'data/my_ads_accounts.html', context = {'LWA_CLIENT_ID': LWA_CLIENT_ID, 'DOMAIN_URL': DOMAIN_URL})
 
     else:
         return render(request, 'subscriptions/subscription_gated_page.html')
