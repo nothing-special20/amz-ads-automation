719d4d9a449dcf9c956d2a840f0743c0433b48d0
Rob <>
2022-06-27 22:16:04 -0400

    fix api bugs - poorly configured view, fns, urls

--- api/functions.py
+++ api/functions.py
@@ -1,6 +1,3 @@
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
-from django.shortcuts import render
 import requests
 import os
 
@@ -27,20 +24,3 @@ def amz_refresh_token(code):
     auth_response = requests.post(amazon_auth_url, data=data)
 
     print(auth_response.text)
-
-def handle_login(request):
-    print('~~~~~~~~weeeeeeeee~~~~~~~~')
-    print(request.method)
-    print('~~~~~~~~weeeeeeeee~~~~~~~~')
-    if request.method == 'GET':
-        print(request)
-        url = request.build_absolute_uri()
-        parsed_url = urlparse(url)
-        code = parse_qs(parsed_url.query)['code'][0]
-        amz_refresh_token(code)
-
-    return render(request, 'web/app_home.html', context={
-        'team': 'fixthis',
-        'active_tab': 'dashboard',
-        'page_title': ('fixthis Dashboard') % {'team': 'fixthis'},
-    })
--- api/views.py
+++ api/views.py
@@ -2,11 +2,29 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from django.conf.urls import url
+from django.shortcuts import render
 
-from . import views
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+
+from .functions import amz_refresh_token
+
 
 app_name = 'api'
 
-urlpatterns = [
-    path('', views.index, name='data'),
-]
+def handle_login(request):
+    print('~~~~~~~~weeeeeeeee~~~~~~~~')
+    print(request.method)
+    print('~~~~~~~~weeeeeeeee~~~~~~~~')
+    if request.method == 'GET':
+        print(request)
+        url = request.build_absolute_uri()
+        parsed_url = urlparse(url)
+        code = parse_qs(parsed_url.query)['code'][0]
+        amz_refresh_token(code)
+
+    return render(request, 'web/app_home.html', context={
+        'team': 'fixthis',
+        'active_tab': 'dashboard',
+        'page_title': ('fixthis Dashboard') % {'team': 'fixthis'},
+    })
