229e7e51be6146b018c39b09f2f5087c14c2b240
Rob <>
2022-06-27 23:30:06 -0400

    actually fix lwa (callback at least)

--- api/views.py
+++ api/views.py
@@ -13,25 +13,22 @@ from .functions import amz_refresh_token
 app_name = 'api'
 
 def handle_login(request):
-    print('~~~~~~~~weeeeeeeee~~~~~~~~')
-    print(request.method)
-    print('~~~~~~~~weeeeeeeee~~~~~~~~')
     url = request.build_absolute_uri()
     if request.method == 'POST':
         print(request)
 
-    if request.method == 'GET' and 'callback' in url:
+    if request.method == 'GET':
         print(request)
         parsed_url = urlparse(url)
         redirect_uri = parsed_url.netloc
         code = parse_qs(parsed_url.query)['code'][0]
         amz_refresh_token(code, redirect_uri)
 
-        return render(request, 'web/app_home.html', context={
-            'team': 'fixthis',
-            'active_tab': 'dashboard',
-            'page_title': ('fixthis Dashboard') % {'team': 'fixthis'},
-        })
+    return render(request, 'web/app_home.html', context={
+        'team': 'fixthis',
+        'active_tab': 'dashboard',
+        'page_title': ('fixthis Dashboard') % {'team': 'fixthis'},
+    })
 
 def handle_login_test(request):
     print('~~~~~~~~weeeeeeeee~~~~~~~~')
