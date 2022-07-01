5495aee27fd127017e1d131e95d8b4a5213fcbe6
Rob <>
2022-07-01 18:42:14 -0400

    configure debugger - YUGELY IMPORTANT

--- manage.py
+++ manage.py
@@ -4,6 +4,10 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vyssio.settings')
+    if (os.getenv('DEBUG', 'False') == 'True'):
+        import debugpy
+        debugpy.listen(("0.0.0.0", 3000))
+        print('Debugger Attached!')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
