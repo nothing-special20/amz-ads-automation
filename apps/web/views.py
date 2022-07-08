59b7b20b1a882b66cd4aadf5fafb07f414b1615d
Rob <>
2022-07-08 18:11:02 -0400

    preliminary privacy policy -bad language - improve

--- apps/web/views.py
+++ apps/web/views.py
@@ -35,3 +35,6 @@ def team_home(request, team_slug):
 
 def simulate_error(request):
     raise Exception('This is a simulated error.')
+
+def privacy_policy(request):
+    return render(request, 'web/privacy_policy.html')
